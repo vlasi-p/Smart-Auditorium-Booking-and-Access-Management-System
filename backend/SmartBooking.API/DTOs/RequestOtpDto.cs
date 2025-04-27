befcd310e19544ad9bf0bc05204890211704ab4e
vlasi-p <vlasivlasi123@gmail.com>
2025-04-27 18:31:42 +0400

    Implemeting Authentication System

---
+++ backend/SmartBooking.API/DTOs/RequestOtpDto.cs
@@ -0,0 +1,4 @@
+public class RequestOtpDto 
+{
+    public string Email { get; set; }
+}
---
+++ backend/SmartBooking.API/DTOs/VerifyOtpDto.cs
@@ -0,0 +1,5 @@
+public class VerifyOtpDto
+{
+    public string Email { get; set; }
+    public string Otp { get; set; }
+}
